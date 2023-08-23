d6d6e909d6da28685dd6a5b6449c48086448f9d5
nirajsakhare0033 <kangthecon0111@gmail.com>
2023-08-23 13:55:09 +0530

    project created for angular learning start from life cycle

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LifeCycleComponent } from './life-cycle/life-cycle.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LifeCycleComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
