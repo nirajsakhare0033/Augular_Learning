6ecbcfeab7e90688f8db5afc283e6769995eb62a
nirajsakhare0033 <kangthecon0111@gmail.com>
2023-08-24 22:37:45 +0530

    ngOnChanges inplemented

--- src/app/child-ngonchange/child-ngonchange.component.ts
+++ src/app/child-ngonchange/child-ngonchange.component.ts
@@ -1,13 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, SimpleChange, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-child-ngonchange',
   templateUrl: './child-ngonchange.component.html',
-  styleUrls: ['./child-ngonchange.component.css']
+  styleUrls: ['./child-ngonchange.component.css'],
 })
 export class ChildNgonchangeComponent {
-@Input() userinput:string='';
-constructor(){}
- 
+  constructor() {
+    console.log('constructor call');
+  }
+  @Input() userinput: string = '';
+  ngOnInit() {
+    console.log('ngOnInit call');
+  }
 
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges call');
+    console.log(changes['userinput'].currentValue);
+  }
 }
